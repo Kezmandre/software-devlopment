9ce900448ba32b3a93ea720b5f443ef085aa8ec7
Quadri <kazeemquadri75@gmail.com>
2024-07-28 23:00:06 +0100

    javascript

--- Quadri/javascript/string.js
+++ Quadri/javascript/string.js
@@ -29,9 +29,7 @@
 // console.log(language.substr(4, 6));
 // console.log (country.substr(3, 6));
 
-
 // substring: takes two argument the starting index and the stopping index, but the stopping index doesnt include the character of the last letter
-
 
 // console.log(language.substring(0,4))
 // console.log(country.substring(0,5))
@@ -39,34 +37,77 @@
 
 //  split: to convert strings into array
 
-let string = "  The olympics has started  "
+// let string = "The olympics has started"
 // let skills = "Express"
 
 // console.log(string.split())
 // console.log(string.split(" "))
 // console.log(skills.split(""))
 
-
 //  trim : to remove trailing or white spaces
 
-
-console.log(string.trim(), "string")
-
+// console.log(string.trim(), "string")
 
 //  includes : returns booleans if a string is present
 
-let present = "The path is clear"
+// let present = "The path is clear"
 
 // console.log(present.includes("path"))
 // console.log(present.includes("clear"))
 // console.log(present.includes("welcome"))
 
-
 // replace : take 2 argument, the old substring and the new string
 
-
-console.log(present.replace("clear", "vague"))
+// console.log(present.replace("clear", "vague"))
 
 // charAt : return the character at a given index
 
-console.log(present.charAt(6))
+// console.log(present.charAt(6))
+
+// charCodeAt
+
+let code = "Learn";
+let char = "learn";
+
+// console.log(code.charCodeAt(0))
+// console.log(char.charCodeAt(0))
+
+// indexOf: this takes a substring  and if the substring exist it returns the first occurrence index of the string and if it does not it returns -1
+
+let language = "let learn javascript learn java";
+
+// console.log(language.indexOf("l"));
+// console.log(language.indexOf("learn"));
+// console.log(language.indexOf("java"));
+// console.log(language.indexOf("knowledge"));
+
+// lastIndexOf:
+
+// console.log(language.lastIndexOf("j"))
+// console.log(language.lastIndexOf("a"))
+
+// concat : this is use to join two or more strings together
+
+// let word1 = "javascript"
+// let word2 = "basics"
+
+// console.log(word1.concat( " ",word2))
+
+// startswith: takes  substring as an argument and check if the substring start with the specified letter. it returns boolean,true if the substring start and false if it does not
+
+
+// console.log(language.startsWith("j"))
+// console.log(language.startsWith("l"))
+
+let result = []
+let countries = ["Nigeria", "Ghana", "USA", "Namibia", "Andorra", "Niger"]
+
+for(let i = 0; i < countries.length; i++)
+    if(countries[i].startsWith("N")){
+        result.push(countries[i])
+    }
+    
+
+// result = countries.filter(country => country.startsWith("N"))
+
+console.log(result)
